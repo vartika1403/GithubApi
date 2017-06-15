c6549006a533d4896acbd65385d26e312dee6c02
vartika1403 <vartika.1403@gmail.com>
2017-06-15 13:47:06 +0530

    renaming items

--- app/src/main/java/com/example/vartikasharma/githubapi/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/githubapi/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.vartikasharma.githubapi;
 
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;

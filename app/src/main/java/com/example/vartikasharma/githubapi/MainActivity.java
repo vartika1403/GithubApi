866de6c3a478eabfb639e00a9a70e9e0353ef0b0
vartika1403 <vartika.1403@gmail.com>
2017-06-15 12:34:26 +0530

    search menu added

--- app/src/main/java/com/example/vartikasharma/githubapi/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/githubapi/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.vartikasharma.githubapi;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -84,5 +91,25 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.search_menu, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuItem searchItem = menu.findItem(R.id.search);
+
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        if (searchView != null) {
+            searchView.setOnQueryTextListener((SearchView.OnQueryTextListener) this);
+        }
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+       // searchView.setOnQueryTextListener(this);
+        return true;
     }
 }
